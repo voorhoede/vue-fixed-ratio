4a9f17a7b16e25d32aff5bc945bae80a6b168eba
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2019-09-12 09:45:44 +0200

    use npm ci instead of install

--- src/vue-fixed-ratio.vue
+++ src/vue-fixed-ratio.vue
@@ -1,10 +1,47 @@
-
 <template>
-  <div>@voorhoede/vue-fixed-ratio</div>
+  <div class="vue-fixed-ratio" :style="{ paddingBottom: `${ratio}%` }">
+    <div class="vue-fixed-ratio__content">
+      <slot></slot>
+    </div>
+  </div>
 </template>
 
 <script>
+const dimension = { type: Number, required: true }
 export default {
-  name: 'VueFixedRatio',
+  props: {
+    height: {
+      type: Number,
+      required: true 
+    },
+    width:  {
+      type: Number,
+      required: true 
+    },
+  },
+  computed: {
+    ratio() {
+      return this.height / this.width * 100
+    }
+  },
 }
+
 </script>
+
+<style>
+.vue-fixed-ratio {
+  display: block;
+  position: relative;
+  height: 0;
+  overflow: hidden;
+}
+
+.vue-fixed-ratio__content {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  max-width: inherit;
+}
+</style>
